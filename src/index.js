0cd1ba2ca37fcdfd8cfdb27ed43150d2ba2fe465
oll310e <o.anderzen@gmail.com>
2023-02-27 12:39:57 +0100

    Add eventlisteners in menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import DOM from "./DOM";
+import mainContent from "./displayMain";
+import clearMain from "./clearMain";
 import "./styles/style.css";
 
-console.log(DOM.getMenu());
+mainContent.displayHome();
+
+const menu = DOM.getMenu();
+
+const homeBtn = menu.querySelector("#home");
+const todayBtn = menu.querySelector("#today");
+const weekBtn = menu.querySelector("#week");
+
+homeBtn.addEventListener("click", (e) => {
+  clearMain();
+  mainContent.displayHome();
+});
+
+todayBtn.addEventListener("click", (e) => {
+  clearMain();
+  mainContent.displayToday();
+});
+
+weekBtn.addEventListener("click", (e) => {
+  clearMain();
+  mainContent.displayWeek();
+});
