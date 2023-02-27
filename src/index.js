6119137aeba4154991c8c95d2c0646ac0a72e0a2
oll310e <o.anderzen@gmail.com>
2023-02-27 14:11:17 +0100

    Add eventlistener for task deleteBtn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import DOM from "./DOM";
 import mainContent from "./displayMain";
 import clearMain from "./clearMain";
+import taskStorage from "./taskStorage";
 import "./styles/style.css";
 
 mainContent.displayHome();
 
 const menu = DOM.getMenu();
+const main = DOM.getMain();
 
 const homeBtn = menu.querySelector("#home");
 const todayBtn = menu.querySelector("#today");
@@ -24,4 +26,14 @@ todayBtn.addEventListener("click", (e) => {
 weekBtn.addEventListener("click", (e) => {
   clearMain();
   mainContent.displayWeek();
+});
+
+main.addEventListener("click", (e) => {
+  if (e.target.classList.contains("deleteBtn")) {
+    taskStorage.removeTask(
+      e.target.parentNode.querySelector(".name").innerText
+    );
+    clearMain();
+    mainContent.displayHome();
+  }
 });
